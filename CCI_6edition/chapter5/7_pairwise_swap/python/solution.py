dac8f52e4e00d729e49ec6d8b3272921ec3ae09a
Vincenzo <vincenzorm117@gmail.com>
2017-01-21 19:20:36 -0500

    added my solution to problem 7 chapter 5

--- CCI_6edition/chapter5/7_pairwise_swap/python/solution.py
+++ CCI_6edition/chapter5/7_pairwise_swap/python/solution.py
@@ -2,6 +2,17 @@
 
 # Copy bin(10) and bin(01) with bin(11) (or int(3))
 
+# My solution
+def pairwise_swap2(N):
+	c = N
+	buff = i = 0
+	while 0 < c:
+		curr = c & 3
+		if curr == 1 or curr == 2:
+			buff = buff | 3 << i
+		i += 2
+		c >>= 2
+	return N ^ buff
 
 def pairwise_swap(N):
 	return ( ((N & 0xaaaaaaaaaaaaaaaaa) >> 1)  | ((N & 0x55555555555555555) << 1))
