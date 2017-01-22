9fa552495c702fb8696ba81fa5dc15f752705a3f
Vincenzo <vincenzorm117@gmail.com>
2017-01-21 19:26:16 -0500

    added alternate solution to problem 1 of chapter 16 that only works on ints

--- CCI_6edition/chapter16/1_number_swapper/python/solution.py
+++ CCI_6edition/chapter16/1_number_swapper/python/solution.py
@@ -7,6 +7,11 @@ def number_swapper(a,b):
 	a ^= b
 	return (a,b)
 
-
+def number_swapper_ints_only(a,b):
+	a = a + b
+	b = a - b
+	a = a - b
+	return (a,b)
 
 print((1,2), number_swapper(1,2))
+print((1,2), number_swapper_ints_only(1,2))
