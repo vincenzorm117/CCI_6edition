9f136a90c0d1f93304873239a1fcb66006d000fe
Vincenzo Marconi <vincenzomarconi@Vincenzos-Mac-mini.local>
2017-02-23 21:41:56 -0500

    completed book solution for chapter 1 problem 3

--- CCI_6edition/chapter1/3_urlify/python/solution.py
+++ CCI_6edition/chapter1/3_urlify/python/solution.py
@@ -1,10 +1,33 @@
 #!/usr/local/bin/python3
 
 
+def urlify_better(s):
+	return s.replace(' ', '%20')
+
 
 def urlify(s):
-	return s.replace(' ','%20')
+	print(s)
+	s = list(s) + ([' '] * (s.count(' ') * 2))
+	L = len(s)
+	last = L - 1
+
+	# Find first non-white-space character from the right
+	c = last
+	while s[c] == ' ' and 0 <= c:
+		c -= 1
+
+	while 0 <= c:
+		if s[c] == ' ':
+			s[last] = '0'
+			s[last - 1] = '2'
+			s[last - 2] = '%'
+			last -= 3
+		else:
+			s[last] = s[c]
+			last -= 1
+		c -= 1
+	return ''.join(s)
+
 
 
 print(urlify(input()))
-
