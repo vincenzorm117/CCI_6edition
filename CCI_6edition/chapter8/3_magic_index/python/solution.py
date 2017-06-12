d3efa3f0c8e6115ed563aeabd9e307b6a09d2d44
Vincenzo <vincenzorm117@gmail.com>
2017-06-11 22:29:31 -0400

    Added solution for problem set that includes duplicates

--- CCI_6edition/chapter8/3_magic_index/python/solution.py
+++ CCI_6edition/chapter8/3_magic_index/python/solution.py
@@ -1,33 +1,59 @@
 #!/usr/local/bin/python3
 
 
+def brute_force(A):
+	for i in range(len(A)):
+		if A[i] == i:
+			return i
+	return None
+
+
+# def find_magic_index_with_dups(A):
+# 	L = len(A)
+# 	if L <= 0 or (L-1) < A[0] or A[L-1] < 0:
+# 		return None
+# 	curr = 0
+# 	# Find first non-negative value
+# 	if A[0] < 0:
+# 		low, high = 0, L
+# 		while low < high:
+# 			curr = int((low+high)/2)
+# 			if A[curr] < 0:
+# 				low = int(((curr+1)+high)/2)
+# 			else:
+# 				high = int((low+curr)/2)
+# 		curr = low
+# 	else:
+# 		curr = 0
+# 	while curr < L:
+# 		val = A[curr]
+# 		if curr == val:
+# 			return curr
+# 		if curr < val:	
+# 			curr = val
+# 		else:
+# 			curr = curr+1
+# 	return None
 
 def find_magic_index_with_dups(A):
-	L = len(A)
-	if L <= 0 or (L-1) < A[0] or A[L-1] < 0:
+	if A is None or len(A) == 0:
 		return None
-	curr = 0
-	# Find first non-negative int
-	if A[0] < 0:
-		low, high = 0, L
-		while low < high:
-			curr = int((low+high)/2)
-			if A[curr] < 0:
-				low = int(((curr+1)+high)/2)
-			else:
-				high = int((low+curr)/2)
-		curr = low
-	else:
-		curr = 0
-	while curr < L:
-		val = A[curr]
-		if curr == val:
+	L = len(A)
+	q = [(0, L-1)]
+	while 0 < len(q):
+		c = q.pop()
+		curr = int((c[0]+c[1])/2)
+		if A[curr] == curr:
 			return curr
-		if curr < val:	
-			curr = val
-		else:
-			curr = curr+1
+		left = min(curr - 1, A[curr])
+		right = max(curr + 1, A[curr])
+		if 0 <= left and c[0] <= left:
+			q.append( (c[0], left) )
+		if right < L and right <= c[1]:
+			q.append( (right, c[1]) )
 	return None
+
+
 
 
 def find_magic_index(A):
@@ -53,14 +79,36 @@ from random import randint
 def gen_array():
 	L = randint(5, 20)
 	A = [0] * L
-	A[0] = randint(-20,0)
+	A[0] = randint(1,10)
 	for i in range(1,L):
-		A[i] = A[i-1] + randint(1,10)
+		A[i] = A[i-1] + randint(0,3)
 	return A
 
 
 
-for _ in range(100):
-	A = gen_array()
+
+
+testcases = [
+	([1,3,3,4,5,5], 5),
+	([0], 0),
+	([1], None),
+	([], None),
+	([1,2,3,4,5], None)
+]
+
+for t in testcases:
+	A = t[0]
 	print(A)
-	print(find_magic_index(A))
+	sol = find_magic_index_with_dups(A)
+	assert(sol == t[1])
+
+
+
+
+# for _ in range(100):
+# 	A = gen_array()
+# 	sol = find_magic_index_with_dups(A)
+# 	if sol is not None:
+# 		print(A)
+# 		print(sol)
+
