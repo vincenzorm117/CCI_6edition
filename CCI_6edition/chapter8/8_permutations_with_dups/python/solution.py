c23006810227788a15b5ae0dc7859faef14c92cc
Vincenzo <vincenzorm117@gmail.com>
2017-07-02 19:41:41 -0400

    Completed chapter 8 problem 8

---
+++ CCI_6edition/chapter8/8_permutations_with_dups/python/solution.py
@@ -0,0 +1,52 @@
+#!/usr/local/bin/python3
+
+
+
+def permute(l):
+	if not isinstance(l, (list)):
+		return None
+	if len(l) <= 0:
+		return []
+	L = len(l)
+	stack = [(l,0)]
+	sols = set()
+	while 0 < len(stack):
+		c = stack.pop()
+		if c[1] == L:
+			sol = []
+			for i in c[0]:
+				sol.append(i)
+			print(sol,tuple(sol))
+			sols.add(tuple(sol))
+		else:
+			for i in range(c[1],L):
+				c[0][c[1]], c[0][i] = c[0][i], c[0][c[1]]
+				stack.append((c[0][:], c[1]+1))
+				c[0][c[1]], c[0][i] = c[0][i], c[0][c[1]]
+	return sols
+
+
+ts = [
+	([], []),
+	([1,2], [(1,2),(2,1)]),
+	([1,2,3], [(1,2,3),(2,1,3),(1,3,2),(2,3,1),(3,1,2),(3,2,1)]),
+	([1,1], [(1,1)]),
+	([1,1,2], [(1,1,2),(1,2,1),(2,1,1)]),
+	([1], [(1,)])
+]
+
+for t in ts:
+	q, a = t
+	sol = permute(q)
+	print(q)
+	print(sol)
+	correct = True
+	if len(sol) == len(a):
+		for x in a:
+			if x not in sol:
+				correct = False
+				break
+	else:
+		correct = False
+	assert(correct)
+
