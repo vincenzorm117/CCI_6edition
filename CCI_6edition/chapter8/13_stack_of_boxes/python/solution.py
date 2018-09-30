654b7c3b76b6a52228b25fd6fc1e983081797cfc
Vincenzo Marconi <vincenzorm117@gmail.com>
2018-09-30 17:08:03 -0400

    Made basic solution for problem 13 chapter 8

---
+++ CCI_6edition/chapter8/13_stack_of_boxes/python/solution.py
@@ -0,0 +1,48 @@
+#!/usr/local/bin/python3
+
+
+def stack_of_boxes(boxes):
+    def sob(unused, used, topBox, heightMax, stackHeight):
+        if len(unused) == 0:
+            return sum([h[1] for h in used])
+        if topBox == None:
+            for box in unused:
+                unused.remove(box)
+                used.add(box)
+                heightMax = sob(unused, used, box, heightMax, stackHeight + box[1])
+                unused.add(box)
+                used.remove(box)
+        else:
+            topWidth, topHeight, topDepth = topBox
+            for box in unused:
+                width, height, depth = box
+                m = heightMax
+                if width < topWidth and height < topHeight and depth < topDepth:
+                    unused.remove(box)
+                    used.add(box)
+                    heightMax = sob(unused, used, box, heightMax, stackHeight + box[1])
+                    unused.add(box)
+                    used.remove(box)
+
+        return max(heightMax, stackHeight)
+    if not isinstance(boxes, list):
+        return 0
+    if len(boxes) <= 0:
+        return 0
+    return sob(set(boxes), set(), None, 0, 0)
+
+
+
+testcases = [
+    [
+        (41,59,61),
+        (79,34,43),
+        (90,49,69),
+        (54,28,47),
+        (31,75,34)
+    ]
+]
+
+for boxes in testcases:
+    # print(boxes)
+    print(stack_of_boxes(boxes))
