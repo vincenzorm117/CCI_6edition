e6f3645706b02287674a4f93b564e052f1d034ed
Vincenzo Marconi <vincenzorm117@gmail.com>
2018-09-30 22:30:32 -0400

    Updated solution 1 of problem 13 chapter 8

--- CCI_6edition/chapter8/13_stack_of_boxes/python/solution.py
+++ CCI_6edition/chapter8/13_stack_of_boxes/python/solution.py
@@ -7,9 +7,12 @@ def stack_of_boxes(boxes):
             return sum([h[1] for h in used])
         if topBox == None:
             for box in unused:
+                # Move box from unused to used
                 unused.remove(box)
                 used.add(box)
+                # Recurse
                 heightMax = sob(unused, used, box, heightMax, stackHeight + box[1])
+                # Move box back to unused category
                 unused.add(box)
                 used.remove(box)
         else:
@@ -18,13 +21,16 @@ def stack_of_boxes(boxes):
                 width, height, depth = box
                 m = heightMax
                 if width < topWidth and height < topHeight and depth < topDepth:
+                    # Move box from unused to used
                     unused.remove(box)
                     used.add(box)
+                    # Recurse
                     heightMax = sob(unused, used, box, heightMax, stackHeight + box[1])
+                    # Move box back to unused category
                     unused.add(box)
                     used.remove(box)
-
         return max(heightMax, stackHeight)
+        
     if not isinstance(boxes, list):
         return 0
     if len(boxes) <= 0:
@@ -40,9 +46,23 @@ testcases = [
         (90,49,69),
         (54,28,47),
         (31,75,34)
+    ],
+    [
+        (0,0,0),
+        (1,1,1),
+        (2,2,2),
+        (3,3,3),
+        (4,4,4),
+    ],
+    [
+        (0,0,0),
+        (1,1,1),
+        (2,2,2),
+        (3,3,0),
+        (4,4,4),
     ]
 ]
 
 for boxes in testcases:
-    # print(boxes)
+    print(boxes)
     print(stack_of_boxes(boxes))
