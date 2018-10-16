e20d2a5193795d16cab3386668b3600150ad6aaa
Vincenzo Marconi <vincenzorm117@gmail.com>
2018-10-15 21:31:11 -0400

    Problem 14 chapter 8. Added memoization

--- CCI_6edition/chapter8/14_boolean_evaluation/python/solution.py
+++ CCI_6edition/chapter8/14_boolean_evaluation/python/solution.py
@@ -52,6 +52,47 @@ def num_boolean_evaluations(expression, answer):
 
 
 
+def num_boolean_evaluations2(expression, answer):
+    values, operators = parse_boolean(expression)
+
+    def recurse(start, end, values, operators, memoize):
+        if start == end:
+            if values[start] == 1:
+                return (0,1)
+            else:
+                return (1,0)
+        if (start, end) in memoize:
+            return memoize[(start, end)]
+
+        ones, zeros = 0, 0
+        for i in range(start, end):
+            leftZeros, leftOnes = recurse(start, i, values, operators, memoize)
+            rightZeros, rightOnes = recurse(i+1, end, values, operators, memoize)
+
+            if operators[i] == 0: # & AND
+                zeros += leftOnes * rightZeros + leftZeros * rightZeros + leftZeros * rightOnes
+                ones += leftOnes * rightOnes
+            elif operators[i] == 1: # | OR
+                zeros += leftZeros * rightZeros
+                ones += leftOnes * rightOnes + leftOnes * rightZeros + leftZeros * rightOnes
+            elif operators[i] == 2: # ^ XOR
+                zeros += leftZeros * rightZeros + leftOnes * rightOnes
+                ones += leftZeros * rightOnes + leftOnes * rightZeros
+            else:
+                raise ValueError('Invalid operator')
+
+        memoize[(start,end)] = (zeros, ones)
+        return (zeros, ones)
+            
+    memoize = {}
+    zeros, ones = recurse(0, len(values)-1, values, operators, memoize)
+    if answer:
+        return ones
+    else:
+        return zeros
+
+
+
 testcases = [
     ("1^0|0|1", False),
     ("0&0&0&1^1|0", True)
@@ -61,3 +102,4 @@ testcases = [
 for expression, answer in testcases:
     print('Case: %s %s' % (expression, answer))
     print(num_boolean_evaluations(expression, answer))
+    print(num_boolean_evaluations2(expression, answer))
