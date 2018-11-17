7644c44c9e4d14fc72cc119c2614d9369f4f79a8
Vincenzo Marconi <vincenzorm117@gmail.com>
2018-11-17 12:43:24 -0500

    Completed problem 3 of chapter 10

--- CCI_6edition/chapter10/3_search_in_rotated_array/python/solution.py
+++ CCI_6edition/chapter10/3_search_in_rotated_array/python/solution.py
@@ -2,15 +2,18 @@
 
 
 def search(array, value):
-    # Do binary search to find offset
+    # Do binary search to find offset)
     low, high = 0, len(array) - 1
-    while low < high:
-        mid = int((low + high)/2)
-        if array[low] > array[mid]:
-            high = mid
-        else:
-            low = mid+1
-    offset = low
+    if array[low] > array[high]:
+        while low < high:
+            mid = int((low + high)/2)
+            if array[low] > array[mid]:
+                high = mid
+            else:
+                low = mid+1
+        offset = (low) % len(array)
+    else:
+        offset = 0
     # Do binary search to find index of input
     low, high = 0, len(array)
     while low < high:
@@ -24,17 +27,36 @@ def search(array, value):
 
 
 
-
-
 testcases = [
-    ([15,16,19,20,25,1,3,4,5,7,10,14], 5, 8)
+    ([15,16,19,20,25,1,3,4,5,7,10,14], 5, 8),
+    ([15,16,19,20,25,1,3,4,5,7,10,14], 7, 9),
+    ([15,16,19,20,25,1,3,4,5,7,10,14], 10, 10),
+    ([15,16,19,20,25,1,3,4,5,7,10,14], 14, 11),
+    ([15,16,19,20,25,1,3,4,5,7,10,14], 15, 0),
+    ([15,16,19,20,25,1,3,4,5,7,10,14], 16, 1),
+    ([15,16,19,20,25,1,3,4,5,7,10,14], 19, 2),
+    ([15,16,19,20,25,1,3,4,5,7,10,14], 20, 3),
+    ([15,16,19,20,25,1,3,4,5,7,10,14], 25, 4),
+    
+
+    ([1,3,4,5,7,10,14,15,16,19,20,25], 1, 0),
+    ([1,3,4,5,7,10,14,15,16,19,20,25], 3, 1),
+    ([1,3,4,5,7,10,14,15,16,19,20,25], 4, 2),
+    ([1,3,4,5,7,10,14,15,16,19,20,25], 5, 3),
+    ([1,3,4,5,7,10,14,15,16,19,20,25], 7, 4),
+    ([1,3,4,5,7,10,14,15,16,19,20,25], 10, 5),
+    ([1,3,4,5,7,10,14,15,16,19,20,25], 14, 6),
+    ([1,3,4,5,7,10,14,15,16,19,20,25], 15, 7),
+    ([1,3,4,5,7,10,14,15,16,19,20,25], 16, 8),
+    ([1,3,4,5,7,10,14,15,16,19,20,25], 19, 9),
+    ([1,3,4,5,7,10,14,15,16,19,20,25], 20, 10),
+    ([1,3,4,5,7,10,14,15,16,19,20,25], 25, 11),
+    
 ]
 
-array, value, solution = ([15,16,19,20,25,1,3,4,5,7,10,14], 5, 8)
-for a in array:
-    print('Case: %s' % a)
-    print(search(array, a))
 
-# for case, value, solution in testcases:
-#     print('Case: %s' % case)
-#     print(search(case, value), solution)
+for case, value, solution in testcases:
+    print('Case: %s' % case)
+    answer = search(case, value)
+    print(answer, solution)
+    assert(solution == answer)
