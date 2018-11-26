3e69bc064c47edb0925e0bb8bd3495496e485057
Vincenzo Marconi <vincenzorm117@gmail.com>
2018-11-25 22:49:07 -0500

    Patch problem 3 chapter 16

--- CCI_6edition/chapter16/3_intersection/python/solution.py
+++ CCI_6edition/chapter16/3_intersection/python/solution.py
@@ -8,8 +8,14 @@ class Point:
     def __str__(self):
         return '(%s,%s)' % (self.x, self.y)
 
+def isBetween(start, mid, end):
+    return (start.x <= mid.x and mid.x <= end.x and start.y <= mid.y and mid.y <= end.y) or (end.x <= mid.x and mid.x <= start.x and end.y <= mid.y and mid.y <= start.y)
 
 def intersection(A1, A2, B1, B2):
+    # Check if line segments could not intersect
+    if not isBetween(A1, B1, A2) and not isBetween(A1, B2, A2) and not isBetween(B1, A1, B2) and not isBetween(B1, A2, B2):
+        return None
+
     dAx = (A2.x - A1.x)
     dAy = (A2.y - A1.y)
     dBx = (B2.x - B1.x)
@@ -23,6 +29,13 @@ def intersection(A1, A2, B1, B2):
         else:
             return None
     else:
+        if dAx == 0:
+            y = A1.y
+            return ((y - (B1.y - (dBy/dBx)*B1.x))/(dBy/dBx), y)
+        if dBx == 0:
+            y = B1.y
+            return ((y - (A1.y - (dAy/dAx)*A1.x))/(dAy/dAx), y)
+
         # Points must intersect, find x that matches both
         x = (B1.y - (dBy/dBx)*B1.x - A1.y + (dAy/dAx)*A1.x) / ((dAy/dAx) - (dBy/dBx))
         return (x, x*(dBy/dBx) + B1.y - (dBy/dBx)*B1.x)
@@ -33,9 +46,16 @@ testcases = [
     (Point(0,0),Point(1,1),Point(0,2),Point(1,3), None),
     (Point(0,0),Point(1,1),Point(0,0),Point(1,1), None),
     (Point(0,0),Point(1,1),Point(0,0),Point(1,3), None),
+    (Point(-1,1),Point(1,-1),Point(-1,-1),Point(1,1), None),
+    (Point(0,-4),Point(0,4),Point(0,-1),Point(0,1), None),
+    (Point(0,-4),Point(0,4),Point(1,-1),Point(1,1), None),
+    (Point(0,0),Point(1,1),Point(1,0),Point(1,1), None),
+    (Point(0,0),Point(2,2),Point(1,0),Point(1,3), None),
+    (Point(0,0),Point(1,1),Point(2,2),Point(3,5), None),
 ]
 
 for A1, A2, B1, B2, answer in testcases:
     print(A1, A2, B1, B2)
     solution = intersection(A1, A2, B1, B2)
     print(solution)
+    print()
