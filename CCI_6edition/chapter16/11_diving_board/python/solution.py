e4916a6e8e2f27d70ea2a7c3fdb6bcdf766947c2
Vincenzo <vincenzorm117@gmail.com>
2020-04-12 20:04:59 -0400

    Solved chapter 16 problem 11

--- CCI_6edition/chapter16/11_diving_board/python/solution.py
+++ CCI_6edition/chapter16/11_diving_board/python/solution.py
@@ -2,25 +2,24 @@
 
 
 
-def divingBoard(K):
-    def recurse(k, freq, boards):
-        if k == 0:
-            print(' '.join(boards))
-            return
-        for key, value in freq.items():
-            if value <= 0:
-                continue
-            freq[key] -= 1
-            boards.append(key)
-            recurse(k - 1, freq, boards)
-            boards.pop()
-            freq[key] += 1
-    
-    freq = {'longer': K, 'shorter': K}
-    recurse(K, freq, [])
+def printDivingBoardLengths(shorter, longer, k):
+    if shorter == longer:
+        return print(shorter * k)
+    if shorter == 0:
+        return print(longer * k)
+    if longer == 0:
+        return print(shorter * k)
+    for i in range(k+1):
+        print(shorter*i + longer*(k - i))
 
 
-for i in range(10):
-    print(i)
-    divingBoard(i)
-    print()
+
+for shorter in range(10):
+    for longer in range(10):
+        for k in range(10):
+            print('============')
+            print(shorter, end=' ')
+            print(longer, end=' ')
+            print(k)
+            print('============')
+            printDivingBoardLengths(shorter, longer, k)
