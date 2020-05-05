41290e2469595e496b73d8c6b986769c764893d0
Vincenzo <vincenzorm117@gmail.com>
2020-05-04 21:43:46 -0400

    Solved chapter 8 problem 9

--- CCI_6edition/chapter8/9_parens/python/solution.py
+++ CCI_6edition/chapter8/9_parens/python/solution.py
@@ -1,50 +1,105 @@
 #!/usr/local/bin/python3
 
 
-T = 6
-
-def printNPairsOfParentheses(N):
-    nPairsOfParens = [['']]
-    s = set()
-    for i in range(1,N+1):
-        s.clear()
-        for option in nPairsOfParens[i-1]:
-            s.add('('+option+')')
-            s.add('()'+option)
-            s.add(option+'()')
-        nPairsOfParens.append(list(s))
-    return nPairsOfParens
-        
-
-options = printNPairsOfParentheses(T)
-# for i in range(len(options)):
-#     print(i, '|', len(options[i]))
-#     for s in options[i]:
-#         print(s)
+flatten = lambda l: [item for sublist in l for item in sublist]
 
 
-def addParen(ls, leftRem, rightRem, s, index):
-    if leftRem < 0 or rightRem < leftRem: 
-        return
 
-    if leftRem == 0 and rightRem == 0:
-        ls.append(''.join(s))
-    else:
-        s[index] = '('
-        addParen(ls, leftRem - 1, rightRem, s, index+1)
+class ParensIteratorWithSet:
 
-        s[index] = ')'
-        addParen(ls, leftRem, rightRem - 1, s, index+1)
+    def __init__(self):
+        self.currN = -1
+        self.currParens = ['']
 
-def generateParens(count):
-    s = list(' '*(count*2))
-    l = list()
-    addParen(l, count, count, s, 0)
-    return l
+    def hasNext(self):
+        return True
 
-for i in range(T):
-    sol = generateParens(i)
-    print(len(options[i]), len(sol))
-    print(sol)
+    # @return (parenCount, answerCount, count)
+    def next(self):
+        self.currN += 1
 
-(((()))), ((()())), ((())()), ((()))(), (()(())), (()()()), (()())(), (())(()), (())()(), ()((())), ()(()()), ()(())(), ()()(()), ()()()(),
+        if self.currN < 1:
+            return (self.currN, 0, [])
+
+        newParens = set()
+        for paren in self.currParens:
+            newParens.add('(%s)' % (paren))
+            newParens.add('()%s' % (paren))
+            newParens.add('%s()' % (paren))
+
+        self.currParens = list(newParens)
+        return (self.currN, len(self.currParens), self.currParens)
+
+
+
+
+class ParensIteratorWithFib:
+
+    def __init__(self):
+        self.currN = -1
+
+        self.currParens = [
+            ['((()))', '(()())'],
+            ['()(())', '()()()'],
+            ['(())()']
+        ]
+
+    def hasNext(self):
+        return True
+
+    # @return (parenCount, answerCount, count)
+    def next(self):
+        self.currN += 1
+
+        if self.currN < 3:
+            starts = [[], ['()'], ['(())', '()()']]
+            return (self.currN, len(starts[self.currN]), starts[self.currN])
+
+
+        if self.currN == 3:
+            parens = flatten(self.currParens)
+            return (self.currN, len(parens), parens)
+
+        allLastParens = flatten(self.currParens)
+        newCurrParens = [[],[],[]]
+        # Outer
+        for paren in allLastParens:
+            newCurrParens[0].append('(%s)' % (paren))
+        # Left
+        for paren in allLastParens:
+            newCurrParens[1].append('()%s' % (paren))
+        # Right
+        for paren in (self.currParens[0] + self.currParens[2]):
+            newCurrParens[2].append('%s()' % (paren))
+
+        self.currParens = newCurrParens
+
+        parens = flatten(self.currParens)
+        return (self.currN, len(parens), parens)
+
+
+def parenAnswersAreEqual(solFib, solSet):
+    solFibN, solFibLen, solFibArray = solFib
+    solSetN, solSetLen, solSetArray = solSet
+
+    if solFibN != solSetN or solFibLen != solSetLen:
+        return False
+
+    solSetArraySet = set(solSetArray)
+    for paren in solFibArray:
+        if not(paren in solSetArraySet):
+            return False
+        solSetArraySet.remove(paren)
+
+    return len(solSetArraySet) == 0
+
+
+iteratorWithSet = ParensIteratorWithSet()
+iteratorWithFib = ParensIteratorWithFib()
+
+
+for i in range(50):
+    solFib = iteratorWithFib.next()
+    solSet = iteratorWithSet.next()
+    print(solFib[1], solSet[1])
+    assert(parenAnswersAreEqual(solFib, solSet))
