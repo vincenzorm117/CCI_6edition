fea0b550d5820aa48ecb96b121a22f55d0d90df6
Vincenzo <vincenzorm117@gmail.com>
2020-05-10 22:01:42 -0400

    Solved chapter 16 problem 14

--- CCI_6edition/chapter16/14_best_line/python/solution.py
+++ CCI_6edition/chapter16/14_best_line/python/solution.py
@@ -1,73 +1,151 @@
 #!/usr/local/bin/python3
 
 
+
+def gcd(a,b):
+    if not isinstance(a, int) or not isinstance(b, int):
+        raise Exception('Arguments not ints')
+    if a > b:
+        a, b = b, a
+    while a != 0:
+        a, b = b % a, a
+    return b
+
 class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-    
+
+    def __sub__(self, other):
+        return (self.y - other.y, self.x - other.x)
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
     def __str__(self):
-        return '(%s,%s)' % (self.x, self.y)
+        return str((self.x, self.y))
+
+    def __repr__(self):
+        return str((self.x, self.y))
+
 
 class Line:
-    def __init__(self, point1, point2):
-        self.point1 = point1
-        self.point2 = point2
-        self.dx = (point2[0] - point1[0])
-        self.dy = (point2[1] - point1[1])
-        self.points = []
-        self.addPoint(point1)
-        self.addPoint(point2)
+    def __init__(self, pointA, pointB):
+        if not isinstance(pointA, Point) or not isinstance(pointB, Point):
+            raise Exception("Invalid Arguments. Make sure they are two arguments of type Point.")
 
-    def addPoint(self, point):
-        self.points.append(point)
+        if pointA == pointB:
+            raise Exception("Invalid Arguments. Make sure the two points provided are different.")
 
-    def size(self):
-        return len(self.points)
+        self.pointA = pointA
+        self.pointB = pointB
 
-    def pointOnLine(self, point):
-        return point[1] * self.dx + self.dy * self.point1[0] == self.dy * point[0] + self.point1[1] * self.dx
+        dy, dx = pointB - pointA
+        g = gcd(dx, dy)
+        self.dx = int(dx / g)
+        self.dy = int(dy / g)
+        self.Bdx = pointA.y * self.dx - pointA.x * self.dy
+        # Note: Bdx comes from:
+        #   y = mx + B
+        #   y = (dy/dx)x + B
+        #   y(dx) = (dy)x + B(dx)
+
+    def id(self):
+        return (self.dx, self.dy, self.Bdx)
+
+    def __hash__(self):
+        return hash((self.dx, self.dy, self.Bdx))
 
     def __str__(self):
-        return str(self.size()) + ' ' + str(self.points)
+        return '(dx: %s, dy: %s, Bdx: %s)' % (self.dx, self.dy, self.Bdx)
+
+    def __repr__(self):
+        return '(dx: %s, dy: %s, Bdx: %s)' % (self.dx, self.dy, self.Bdx)
+
+    def clone(self):
+        return Line(self.pointA, self.pointB)
 
 
-def GetBestLine(points):
-    if len(points) < 2:
+
+class LineMap:
+
+    def __init__(self, EPSILON):
+        self.EPSILON = EPSILON
+        self.lines = {}
+
+    def add(self, line):
+        for key in self.getLineKeys(line):
+            if key in self.lines:
+                self.lines[key].append(line)
+            else:
+                self.lines[key] = [line]
+
+
+    def getLineKeys(self, line):
+         key0 = (self.round(line.dx), self.round(line.dy), self.round(line.Bdx))
+         key1 = (key0[0] + self.EPSILON, key0[1] + self.EPSILON, key0[2] + self.EPSILON)
+         key2 = (key0[0] - self.EPSILON, key0[1] - self.EPSILON, key0[2] - self.EPSILON)
+         return [hash(key0), hash(key1), hash(key2)]
+
+    def round(self, value):
+        factor = int(value / self.EPSILON)
+        return float(factor) * self.EPSILON
+
+    def getLineWithMostPoints(self):
+        # print(self.lines)
+        lineWithMostPointsList = []
+        for key in self.lines:
+            if len(lineWithMostPointsList) < len(self.lines[key]):
+                lineWithMostPointsList = self.lines[key]
+        if 0 < len(lineWithMostPointsList):
+            return lineWithMostPointsList[0]
+        else:
+            return None
+
+
+
+
+def getBestLine(points):
+    if not (0 < len(points)):
         return None
-    bestLine = None
-    lines = {}
-    for i in range(len(points)-1):
-        for j in range(i+1, len(points)):
-            A,B = points[i], points[j]
-            if (A,B) in lines:
-                continue
-            line = Line(A, B)
-            lines[(A,B)] = line
-            lines[(B,A)] = line
-            for curr in range(j+1, len(points)):
-                C = points[curr]
-                if line.pointOnLine(C):
-                    lines[(B,C)] = line
-                    lines[(C,B)] = line
-                    line.addPoint(C)
-            if bestLine is None or bestLine.size() < line.size():
-                bestLine = line
-    return bestLine
 
+    lines = LineMap(.0001)
+    for i in range(0, len(points)-1):
+        for j in range(1, len(points)):
+            pointA, pointB = points[i], points[j]
+
+            if pointA == pointB:
+                continue
+
+            line = Line(pointA, pointB)
+            lines.add(line)
+
+    lineWithMostPoints = lines.getLineWithMostPoints()
+
+    if lineWithMostPoints == None:
+        somePoint = points[0]
+        otherPoint = Point(somePoint.x + 1, somePoint.y)
+        return Line(somePoint, otherPoint)
+
+    return lineWithMostPoints
 
 
 testcases = [
-    [(0,0)],
-    [(0,0),(1,1)],
-    [(-1,1),(-1,-1),(1,1),(1,-1)],
-    [(0,0),(0,1),(0,1),(3,1)],
-    [(0,0),(0,1),(0,1),(3,1),(3,0),(3,-5),(3,9)],
+    [Point(0,0)],
+    [Point(0,0),Point(1,1)],
+    [Point(-1,1),Point(-1,-1),Point(1,1),Point(1,-1)],
+    [Point(0,0),Point(0,1),Point(0,2),Point(3,1)],
+    [Point(0,0),Point(0,1),Point(0,1),Point(3,1),Point(3,0),Point(3,-5),Point(3,9)],
+    [Point(0,0),Point(0,0),Point(0,0),Point(0,0)],
+    [Point(0,0),Point(0,0),Point(0,0),Point(0,0),Point(1,1),Point(0,1)],
+    [Point(100,20), Point(120,40), Point(-900,-980), Point(100, 30)],
+    [Point(33, 35), Point(37, 36)]
 ]
 
 
 for i in range(len(testcases)):
     case = testcases[i]
     print('Case %s:' % i)
-    print(case, end="\n\n")
-    print(GetBestLine(case))
+    print(case, end="\n")
+    print(getBestLine(case))
+    print()
